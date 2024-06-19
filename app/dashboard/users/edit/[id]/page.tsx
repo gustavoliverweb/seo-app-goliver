965b21533268e24ab8df5972743c5e8275f1f6fe
gustavoliverweb <gustavo@santiagodev.com>
2024-06-19 15:21:51 -0400

    Remove unused component

--- app/dashboard/users/edit/[id]/page.tsx
+++ app/dashboard/users/edit/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { fetchUsersId } from "@/app/lib/data";
-import EditUserForm from "@/app/ui/users/editUserForm";
 import WrapperEditUsers from "@/app/ui/users/wrapperEditUsers";
 
 export default async function Page({ params }: { params: { id: string } }) {
