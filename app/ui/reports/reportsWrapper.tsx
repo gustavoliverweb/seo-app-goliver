1286064803b84e534283ef184403eec3c38d91db
gustavoliverweb <gustavo@santiagodev.com>
2024-06-20 11:17:57 -0400

    Fix styles not reports state

--- app/ui/reports/reportsWrapper.tsx
+++ app/ui/reports/reportsWrapper.tsx
@@ -24,34 +24,9 @@ export default function ReportsWrapper({
   const [reportId, setReportId] = useState("");
   const name = reports.find((report) => report.id === reportId)?.name;
   const deleteReportWithId = deleteReport.bind(null, reportId);
+
   if (query && reports.length === 0) {
     return (
-      <div className="flex flex-grow justify-center items-center text-[24px]">
-        No hay coincidencias con la búsqueda: {query}
-      </div>
-    );
-  }
-
-  if (!reports?.length) {
-    return (
-      <div className="flex flex-grow justify-center items-center">
-        No hay informes creados
-      </div>
-    );
-  }
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Delete report", name);
-    const whitespaceRemoved = name?.replace(/\s/g, "");
-    await fetch(`/api/deleteReportAssets?report=${whitespaceRemoved}`, {
-      method: "POST",
-    });
-    await deleteReportWithId();
-    setShowModal(false);
-    setIsModalDeleteShow(false);
-  };
-  return (
-    <>
       <div className="items-start mt-6 w-full flex flex-col px-5 gap-6">
         <div
           className={clsx(
@@ -84,34 +59,126 @@ export default function ReportsWrapper({
             }
           )}
         >
-          <div className="flex flex-col gap-4 ">
-            {reports &&
-              reports?.map((report) => (
-                <div key={report.id}>
-                  <ReportsCard
-                    id={report.id}
-                    name={report.name}
-                    templateName={report.select_template}
-                    setReportId={setReportId}
-                    setShowModal={setShowModal}
-                  />
-                </div>
-              ))}
-          </div>
-          <ConfirmModal
-            showModal={showModal}
-            setShowModal={setShowModal}
-            handleSubmit={handleSubmit}
-          >
-            ¿Estás seguro de eliminar el reporte{" "}
-            <span className="font-medium">{name}</span>?
-          </ConfirmModal>
-
-          <div className="mt-5 flex w-full ">
-            <Pagination totalPages={reportsPages} />
+          <div className="flex flex-1 justify-center items-center text-[24px] flex-col gap-4 ">
+            No hay coincidencias con la búsqueda: {query}
           </div>
         </div>
       </div>
-    </>
+    );
+  }
+
+  if (!reports?.length) {
+    return (
+      <div className="items-start mt-6 w-full flex flex-col px-5 gap-6">
+        <div
+          className={clsx(
+            "p-5 rounded-2xl lg:flex justify-between items-center w-full transition",
+            {
+              "bg-white": !isDark,
+              "bg-dark-dark-background-panels": isDark,
+            }
+          )}
+        >
+          <h3
+            className={clsx(
+              "text-title font-medium text-primary-text-500 transition",
+              {
+                "text-primary-text-500": !isDark,
+                "text-white": isDark,
+              }
+            )}
+          >
+            Informes
+          </h3>
+          <CreateReport />
+        </div>
+        <div
+          className={clsx(
+            "h-max overflow-auto p-5 rounded-2xl w-full flex-grow flex flex-col justify-between transition",
+            {
+              "bg-white": !isDark,
+              "bg-dark-dark-background-panels": isDark,
+            }
+          )}
+        >
+          <div className="flex flex-1 justify-center items-center text-[24px] flex-col gap-4 ">
+            No hay informes creados
+          </div>
+        </div>
+      </div>
+    );
+  }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Delete report", name);
+    const whitespaceRemoved = name?.replace(/\s/g, "");
+    await fetch(`/api/deleteReportAssets?report=${whitespaceRemoved}`, {
+      method: "POST",
+    });
+    await deleteReportWithId();
+    setShowModal(false);
+    setIsModalDeleteShow(false);
+  };
+  return (
+    <div className="items-start mt-6 w-full flex flex-col px-5 gap-6">
+      <div
+        className={clsx(
+          "p-5 rounded-2xl lg:flex justify-between items-center w-full transition",
+          {
+            "bg-white": !isDark,
+            "bg-dark-dark-background-panels": isDark,
+          }
+        )}
+      >
+        <h3
+          className={clsx(
+            "text-title font-medium text-primary-text-500 transition",
+            {
+              "text-primary-text-500": !isDark,
+              "text-white": isDark,
+            }
+          )}
+        >
+          Informes
+        </h3>
+        <CreateReport />
+      </div>
+      <div
+        className={clsx(
+          "h-max overflow-auto p-5 rounded-2xl w-full flex-grow flex flex-col justify-between transition",
+          {
+            "bg-white": !isDark,
+            "bg-dark-dark-background-panels": isDark,
+          }
+        )}
+      >
+        <div className="flex flex-col gap-4 ">
+          {reports &&
+            reports?.map((report) => (
+              <div key={report.id}>
+                <ReportsCard
+                  id={report.id}
+                  name={report.name}
+                  templateName={report.select_template}
+                  setReportId={setReportId}
+                  setShowModal={setShowModal}
+                />
+              </div>
+            ))}
+        </div>
+        <ConfirmModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          handleSubmit={handleSubmit}
+        >
+          ¿Estás seguro de eliminar el reporte{" "}
+          <span className="font-medium">{name}</span>?
+        </ConfirmModal>
+
+        <div className="mt-5 flex w-full ">
+          <Pagination totalPages={reportsPages} />
+        </div>
+      </div>
+    </div>
   );
 }
